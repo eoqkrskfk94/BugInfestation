7ff1c152acc3dfd89dc4be4001c6bd03730ddc61
Kim Myung Jin <eoqkrskfk94@naver.com>
2018-03-30 11:05:06 +0900

    BugInfestation class

--- src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
+++ src/main/java/edu/handong/csee/java/lab06/BugInfestation.java
@@ -1,4 +1,4 @@
-package edu.handong.csee.java.lab06;
+package edu.handong.csee.java.lab06; //Package
 
 /**
  * This class defines calculator objects<br>
@@ -6,35 +6,33 @@ package edu.handong.csee.java.lab06;
  * methods:<br>
  * Setting values to the variables<br>
  * Calculating needed varialbes using inserted values<br>
-
  * @author Kim Myung Jin 21400082
  *
  */
-
-public class BugInfestation {
+public class BugInfestation { //declaring BugInfestation class
 	
-	static double GROWTH_RATE = 0.95; //declaring variables needed in the methods
-	static double ONE_BUG_VOLUME = 0.002;
-	double housevol;
-	double startpop;
-	double totalbugvol;
-	double newbugs = 0, newbugvol = 0;
-	int countweek = 0;
+	static double GROWTH_RATE = 0.95; //Making a static value where GROWTH_RATE has a value of 0.95
+	static double ONE_BUG_VOLUME = 0.002; //Making a static value where ONE_BUG_VOLUME has a value of 0.002
+	double housevol; //Declare housevol double variable
+	double startpop; //Declare startpop double variable
+	double totalbugvol; //Declare totalbugvol double variable
+	double newbugs = 0, newbugvol = 0; //Declare newbugs,newbugvol int variables with the value of 0
+	int countweek = 0; //Declare countweek int variable with the value of 0
 
 	void Set(double a, double b) //this method sets the inserted value to the variables in this class
 	{
-		housevol = a;
-		startpop = b;
-		totalbugvol = b * ONE_BUG_VOLUME;
+		housevol = a; //Insert the value a into housevol
+		startpop = b; //Insert the value b into startpop
+		totalbugvol = b * ONE_BUG_VOLUME; //insert the value b*0.002 into totalbugvol
 	}
 	
 	void Calculatenewinfo() //This method calculates the given variables
 	{
-		newbugs = startpop * GROWTH_RATE;
-		newbugvol = newbugs * ONE_BUG_VOLUME;
-		startpop = startpop + newbugs;
-		totalbugvol = totalbugvol + newbugvol;
-		countweek = countweek + 1;
+		newbugs = startpop * GROWTH_RATE; //calculates newbugs
+		newbugvol = newbugs * ONE_BUG_VOLUME; //calculates newbugvol
+		startpop = startpop + newbugs; //calculates startpop
+		totalbugvol = totalbugvol + newbugvol; //calculates totalbugvol
+		countweek = countweek + 1; //week value in increased by 1
 	}
 	
 	
