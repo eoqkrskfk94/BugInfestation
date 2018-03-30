3129cb26c03998d54711521915d2326d84e7b020
Kim Myung Jin <eoqkrskfk94@naver.com>
2018-03-30 11:05:14 +0900

    Bug main class

--- src/main/java/edu/handong/csee/java/lab06/Bug_main.java
+++ src/main/java/edu/handong/csee/java/lab06/Bug_main.java
@@ -1,6 +1,6 @@
-package edu.handong.csee.java.lab06;
+package edu.handong.csee.java.lab06; //Package
 
-import java.util.Scanner;
+import java.util.Scanner; //imports the scanner class from Java utility
 
 /**
  * This is the main class for buginfestation 
@@ -8,7 +8,7 @@ import java.util.Scanner;
  * @author Kim Myung Jin 21400082
  *
  */
-public class Bug_main {
+public class Bug_main { //declares bug_main class
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -20,13 +20,13 @@ public class Bug_main {
 		
 		Scanner input = new Scanner(System.in); //creates scanner for insertion
 		
-		System.out.print("Enter the total volume of your house in cubic feet: "); 
+		System.out.print("Enter the total volume of your house in cubic feet: ");  //prints out the given string
 		house_vol = input.nextDouble(); //inserts entered value to the variable
 		
-		System.out.print("Enter estimated number of roaches in your house: "); 
+		System.out.print("Enter estimated number of roaches in your house: "); //prints oute the given string
 		pop = input.nextDouble(); //inserts entered value to the variable
 		
-		bug.Set(house_vol, pop);
+		bug.Set(house_vol, pop); // the method set() in BugInfestation class is called and executed
 		
 		while(bug.totalbugvol < bug.housevol) //repeats the loop until totalbugvol is greater than housevol
 		{
@@ -34,12 +34,12 @@ public class Bug_main {
 		}
 		
 		
-		System.out.println("Starting with a roach population of " + pop); //prints out the given information
-		System.out.println("and a house with a volume of " + bug.housevol + " cubic feet,");
-		System.out.println("after " + bug.countweek + " weeks"); 
-		System.out.printf("the house will be filled with %.0f roaches.\n", bug.startpop);
-		System.out.printf("They will fill a voule of %.0f cubic feet.\n", bug.totalbugvol);
-		System.out.println("Better call Debugging Experts Inc."); 
+		System.out.println("Starting with a roach population of " + pop); //prints out the given information(population)
+		System.out.println("and a house with a volume of " + bug.housevol + " cubic feet,"); //prints out the given information (house volume)
+		System.out.println("after " + bug.countweek + " weeks");  //prints out the given information (weeks)
+		System.out.printf("the house will be filled with %.0f roaches.\n", bug.startpop); //prints out the given information (Start population)
+		System.out.printf("They will fill a voule of %.0f cubic feet.\n", bug.totalbugvol); //prints out the given information (total bug volume)
+		System.out.println("Better call Debugging Experts Inc.");  //prints out the given information
 		
 	}
 
